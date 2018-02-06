8f66eb0785a7424c47d273c6390d600a01866ffc
Robby Abaya <rabaya@gmail.com>
2018-02-06 15:23:54 -0500

    Initial Commit

---
+++ TestOrientation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestOrientation
+//
+//  Created by Robby Abaya on 2/6/18.
+//  Copyright © 2018 rawbee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
